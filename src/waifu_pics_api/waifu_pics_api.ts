aa398cad7d6487e3607882f2ad862051fbd90069
Dokkae6949 <finnliry@gmail.com>
2024-03-05 09:08:41 +0100

    Add back excluded urls param

--- src/waifu_pics_api/waifu_pics_api.ts
+++ src/waifu_pics_api/waifu_pics_api.ts
@@ -11,9 +11,9 @@ export async function fetchWaifuImageUrl(config: { type: Type; category: SfwCate
     }
 }
 
-export async function fetchWaifuImageUrls(config: { type: Type; category: SfwCategory | NsfwCategory }): Promise<string[]> {
+export async function fetchWaifuImageUrls(config: { type: Type; category: SfwCategory | NsfwCategory }, excludedUrls: string[] = []): Promise<string[]> {
     try {
-        return await invoke('get_waifu_image_urls', { config: `{"${config.type.toString()}": "${config.category.toString()}"}`, excluded: []});
+        return await invoke('get_waifu_image_urls', { config: `{"${config.type.toString()}": "${config.category.toString()}"}`, excluded: excludedUrls});
     } catch (error) {
         console.error('Error fetching waifu image URLs:', error);
         throw error;
